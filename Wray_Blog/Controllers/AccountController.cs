d44616bc586fb1ee528295984609be1c47c9679f
Ashton Wray <aricks1986@gmail.com>
2020-04-27 16:51:47 -0400

    update registration form

--- Wray_Blog/Controllers/AccountController.cs
+++ Wray_Blog/Controllers/AccountController.cs
@@ -147,11 +147,21 @@ namespace Wray_Blog.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(ExtendedRegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser 
+                { 
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    DisplayName = model.DisplayName,
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- Wray_Blog/Models/AccountViewModels.cs
+++ Wray_Blog/Models/AccountViewModels.cs
@@ -79,7 +79,29 @@ namespace Wray_Blog.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        
     }
+    // use C# inheritance to extend the RegisterViewModel class
+    public class ExtendedRegisterViewModel : RegisterViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+    }
+
 
     public class ResetPasswordViewModel
     {
