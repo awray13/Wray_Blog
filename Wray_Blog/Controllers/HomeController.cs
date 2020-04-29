45202ac6ad43334a976d4adcbfa7e798ae8982f5
Ashton Wray <aricks1986@gmail.com>
2020-04-29 12:08:03 -0400

    update files
    added email contact functionality

--- Wray_Blog/Controllers/HomeController.cs
+++ Wray_Blog/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using Wray_Blog.Models;
 
@@ -27,9 +31,43 @@ namespace Wray_Blog.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            EmailModel model = new EmailModel();
 
-            return View();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold>({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = $"Ashton's Blog<{WebConfigurationManager.AppSettings["emailfrom"]}>";
+                    model.Body = "This is a message from your portfolio site. The name and the email of the contacting person is above.";
+
+                    var email = new MailMessage(from, WebConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = "Portfolio Contact Email",
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new EmailService();
+                    await svc.SendAsync(email);
+
+                    return View(new EmailModel());
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
         }
     }
 }
--- Wray_Blog/Models/EmailModel.cs
+++ Wray_Blog/Models/EmailModel.cs
@@ -12,9 +12,9 @@ namespace Wray_Blog.Models
         public string FromName { get; set; }
         [Required, Display(Name = "Email"), EmailAddress]
         public string FromEmail { get; set; }
-        [Required]
+        
         public string Subject { get; set; }
-        [Required]
+        
         public string Body { get; set; }
     }
 }
